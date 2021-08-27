04e10440dd9a2d51a62fb0b526b8b474516ec631
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-08-27 18:32:08 +0900

    add: 親コンポーネント, Atomsコンポーネントの読み込み

---
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="article-post">
+    
+  </div>
+</template>
+
+<script>
+import {
+  Heading,
+  Input,
+  Textarea,
+  MarkdownPreview,
+  Button,
+  Select,
+  Text
+} from '@Components/atoms'
+
+export default {
+  components: {
+    heading: Heading,
+    input: Input,
+    textarea: Textarea,
+    markdownPreview: MarkdownPreview,
+    button: Button,
+    Select: Select,
+    text: Text,
+  },
+  props :{
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
