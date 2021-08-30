535dbe6f26658e0f7d0f8e22ba5a58e49ab87ead
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-08-30 15:12:13 +0900

    add: カテゴリー選択機能の実装　親コンポーネント

--- src/js/pages/Articles/Post.vue
+++ src/js/pages/Articles/Post.vue
@@ -1,16 +1,45 @@
 <template lang="html">
   <div>
-    <article-post />
+    <article-post
+      :done-message="doneMessage"
+      :error-message="errorMessage"
+      :selected-category-name="selectedCategoryName"
+      :category-list="categoryList"
+      @selectedCategory="selectedCategory"
+    />
   </div>
 </template>
 <script>
-import { ArticlePost } from '@Components/molecules'
+import { ArticlePost } from '@Components/molecules';
 
 export default {
   components: {
-    articlePost: ArticlePost
+    articlePost: ArticlePost,
   },
-  
-}
+  computed: {
+    doneMessage() {
+      return this.$store.state.articles.doneMessage;
+    },
+    errorMessage() {
+      return this.$store.state.articles.errorMessage;
+    },
+    selectedCategoryName() {
+      return this.$store.state.articles.targetArticle.category.name;
+    },
+    categoryList() {
+      return this.$store.state.categories.categoryList;
+    },
+  },
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
+  },
+  methods: {
+    selectedCategory($event) {
+      const value = $event.target.value;
+      console.log(value);
+      this.$store.dispatch('articles/selectedArticleCategory', value);
+    },
+  },
+};
 
 </script>
