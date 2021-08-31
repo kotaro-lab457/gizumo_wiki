1c921804d6eb059d71fc4eba625d4427996f7e2f
鈴木孝太郎 <k.chanmpmp@gmail.com>
2021-08-31 18:44:40 +0900

    add: 子コンポーネントのスタイリング

--- src/js/components/molecules/ArticlePost/index.vue
+++ src/js/components/molecules/ArticlePost/index.vue
@@ -1,12 +1,17 @@
 <template lang="html">
-  <div>
+  <div class="article-post">
     <div v-if="errorMessage">
       <app-text bg-error>{{ errorMessage }}</app-text>
     </div>
-    <div class="article-post">
-      <section>
+    <div class="article-post__columns">
+      <section class="article-post-editor">
         <app-heading :level="1">記事の新規作成</app-heading>
-        <app-heading :level="2">カテゴリーの選択</app-heading>
+        <app-heading
+          :level="2"
+          class="article-post-editor-title"
+        >
+        カテゴリーの選択
+        </app-heading>
         <app-select
           v-validate="'required'"
           name="category"
@@ -30,27 +35,37 @@
             {{ category.name }}
           </option>
         </app-select>
-        <app-heading :level="2">タイトル・本文</app-heading>
-        <app-input
-          v-validate="'required'"
-          name="title"
-          type="text"
-          placeholder="記事のタイトルを入力してください。"
-          data-vv-as="タイトル"
-          :error-messages="errors.collect('title')"
-          :value="articleTitle"
-          @updateValue="$emit('titleArticle', $event)"
-        />
-        <app-textarea
-          v-validate="'required'"
-          name="content"
-          placeholder="記事の本文をマークダウン記法で入力してください。"
-          data-vv-as="記事の本文"
-          :error-messages="errors.collect('content')"
-          :value="articleContent"
-          @updateValue="$emit('contentArticle', $event)"
-        />
+        <app-heading
+          :level="2"
+          class="article-post-editor-title"
+        >
+        タイトル・本文
+        </app-heading>
+        <div class="article-post-form">
+          <app-input
+            v-validate="'required'"
+            name="title"
+            type="text"
+            placeholder="記事のタイトルを入力してください。"
+            data-vv-as="タイトル"
+            :error-messages="errors.collect('title')"
+            :value="articleTitle"
+            @updateValue="$emit('titleArticle', $event)"
+          />
+        </div>
+        <div class="article-post-form">
+          <app-textarea
+            v-validate="'required'"
+            name="content"
+            placeholder="記事の本文をマークダウン記法で入力してください。"
+            data-vv-as="記事の本文"
+            :error-messages="errors.collect('content')"
+            :value="articleContent"
+            @updateValue="$emit('contentArticle', $event)"
+          />
+        </div>
         <app-button
+          class="article-post-submit"
           round
           type="button"
           :disabled="!disabled"
@@ -59,7 +74,7 @@
           {{ buttonText }}
         </app-button>
       </section>
-      <article>
+      <article class="article-post-preview">
         <app-markdown-preview
           :markdown-content="markdownContent"
         />
@@ -145,6 +160,32 @@ export default {
 
 <style lang="postcss" scoped>
 .article-post {
-  display: flex;
+  &__columns {
+    display: flex;
+    height: 100%;
+  }
+  &-editor {
+    padding-right: 2%;
+    width: 50%;
+    border-right: 1px solid #ccc;
+    &-title {
+      margin-top: 16px;
+    }
+  }
+  &-preview {
+    margin-left: 2%;
+    width: 48%;
+    overflow-y: scroll;
+    background-color: #fff;
+  }
+  &-form {
+    margin-top: 20px;
+  }
+  &-submit {
+    margin-top: 16px;
+  }
+  &__notice--update {
+    margin-bottom: 16px;
+  }
 }
 </style>
